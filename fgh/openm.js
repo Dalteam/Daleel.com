96f186ec3fa7100f90d569b31657406d76d8f73e
DaleelTeam <123378061+Dalteam@users.noreply.github.com>
2023-05-23 22:05:50 +0300

    Delete fgh directory

--- fgh/openm.js
+++
@@ -1,7 +0,0 @@
-
-menu = document.querySelector('.open-menu');
-
-menu.onclick = function(){
-    navlist = document.querySelector('.nav-links');
-    navlist.classList.toggle("open");
-}
--- fgh/routes.js
+++
@@ -1,4 +0,0 @@
-import express from express
-import { searchUser } from "./controllers"
-const router = express.Router();
-router.get('api/search',searchUser)
